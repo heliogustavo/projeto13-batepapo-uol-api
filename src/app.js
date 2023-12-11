43a6b79fb30a8cd7b0ef77c4b6e5752923e45cc4
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2023-12-11 13:33:42 -0400

    setup e instalações

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const app = express();
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+const app = express()
+
+//setup
+app.use(cors())
+app.use(express.json())
+dotenv.config()
 const port = 5000;
 
+//Conexão DB
+const mongoclient = new MongoClient(process.env.DATABASE_URL)
+
+try{
+  mongoclient.connect()
+  console.log("Mongo DB conectado ")
+}catch(err){
+  console.log(err.message)
+
+}
+
+  const db = mongoclient.db()
+
 app.get('/', (req, res) => {
-  res.send('');
+  res.send('');    
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
-});
+});    
