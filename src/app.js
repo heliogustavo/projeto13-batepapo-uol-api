e16ac669fc3419c29390ab72f1d6ae51e994a28e
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2023-12-11 14:14:21 -0400

    Validação Joi config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import Joi from "joi";
 
 const app = express()
 
@@ -19,10 +20,25 @@ try{
   console.log("Mongo DB conectado ")
 }catch(err){
   console.log(err.message)
-
 }
-
   const db = mongoclient.db()
+
+  //schemas
+  const participantschema = Joi.object({name: Joi.string().required()})
+
+  //endpoints
+  app.post("", async(req, res)=> {
+    const {name} = req.body
+    const validation  = participantschema.validate(req.body, {abortEarly:false})
+    if(validation.error){
+     return  res.status(422).send(validation.error.details.map(detail =>detail.message))
+    }
+    try{
+
+    }catch{
+      res.status(500).send(err.message)
+    }
+  })
 
 app.get('/', (req, res) => {
   res.send('');    
